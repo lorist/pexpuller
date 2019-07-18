7d5bdc358b98b4a08671f61c95b2c383c28af45d
Dennis Lorist <dennis@pexip.com>
2019-07-18 17:05:25 +1000

    Update mgr_api.py

--- mgr_api.py
+++ mgr_api.py
@@ -11,6 +11,7 @@ class MgrPull():
         # self.customer = params.get('customer')
         self.values = kwargs['mgr_address']
         self.service_type = kwargs['service_type']
+        self.filter = kwargs['filter']
         self.customer = kwargs['customer']
         self.start = kwargs['start']
         self.now = kwargs['now']
@@ -19,11 +20,11 @@ class MgrPull():
         self.mgr_address = kwargs['mgr_address']
 
     def get_result(self):
-        url = "https://%s/api/admin/history/v1/participant/?limit=5000&has_media=True&call_direction=in&service_type=%s&remote_alias__contains=%s&end_time__gte=%s&end_time__lt=%s" % (self.mgr_address, self.service_type, self.customer, self.start, self.now)
+        url = f"https://{self.mgr_address}/api/admin/history/v1/participant/?limit=5000&has_media=True&call_direction=in&service_type={self.service_type}&{self.filter}__contains={self.customer}&end_time__gte={self.start}&end_time__lt={self.now}"
         response = requests.get(url, auth=HTTPBasicAuth(self.user, self.password))
         json_obj = []
         try:
-            response.raise_for_status()
+            # response.raise_for_status()
             json_obj = response.json()
         except requests.exceptions.HTTPError as e:
             # Whoops it wasn't a 200
@@ -37,6 +38,7 @@ class MgrPull():
 	        	content = json.dumps(json_obj['objects'], indent = 4,sort_keys=True)
 	        	log.info("Got results. %s", total_count)
 	        	return content
+	        	
 	        else:
 	        	log.info("No calls during these times")
 	        	sys.exit()
